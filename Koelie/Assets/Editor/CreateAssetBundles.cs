8123fb62cc459a7581014f9eb1724b94513c82e3
oHanafi <o.hanafi@student.fontys.nl>
2017-10-15 11:36:14 +0200

    update

---
+++ Koelie/Assets/Editor/CreateAssetBundles.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class CreateAssetBundles : MonoBehaviour {
+	[MenuItem ("Assets/Build AssetBundles")]
+	static void BuildAllAssetBundles()
+	{
+		BuildPipeline.BuildAssetBundles ("Assets/AssetBundles", BuildAssetBundleOptions.None, BuildTarget.Android);
+	}
+
+}
